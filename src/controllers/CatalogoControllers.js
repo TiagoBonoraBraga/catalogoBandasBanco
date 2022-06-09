e34b5939ae8a40851fbae5b2b4858e2033d9960f
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-06-09 16:34:47 -0300

    final

--- src/controllers/CatalogoControllers.js
+++ src/controllers/CatalogoControllers.js
@@ -1,11 +1,26 @@
 const res = require("express/lib/response");
 const Bandas = require("../models/Catalogo");
 const Catalogo = require("../models/Catalogo");
+const orderById = { order: [["id", "ASC"]] };
+
+let message = "";
+let type = "";
 
 const getAll = async (req, res) => {
   try {
-    const catalogoBandas = await Catalogo.findAll();
-    res.render("index", { catalogoBandas, bandasPut: null, bandasDel: null });
+    setTimeout(() => {
+      message = "";
+      type = "";
+    }, 1000)
+
+    const catalogoBandas = await Catalogo.findAll(orderById);
+    res.render("index", {
+      catalogoBandas,
+      bandasPut: null,
+      bandasDel: null,
+      message,
+      type,
+    });
   } catch (err) {
     res.status(500).send({ err: err.message });
   }
@@ -13,7 +28,7 @@ const getAll = async (req, res) => {
 
 const singup = (req, res) => {
   try {
-    res.render("signup");
+    res.render("signup", { message, type });
   } catch (err) {
     res.status(500).send({ err: err.message });
   }
@@ -22,11 +37,15 @@ const singup = (req, res) => {
 const create = async (req, res) => {
   try {
     const bandas = req.body;
-    if (!bandas) {
+    if (!bandas.nome || !bandas.descricao || !bandas.estilo || !bandas.imagem) {
+      message = "Preencha todos os campos para cadastrar!";
+      type = "danger";
       return res.redirect("/signup");
     }
 
     await Catalogo.create(bandas);
+    message = "Banda criada com sucesso!";
+    type = "success";
     res.redirect("/");
   } catch (err) {
     res.status(500).send({ err: err.message });
@@ -36,7 +55,7 @@ const create = async (req, res) => {
 const getById = async (req, res) => {
   try {
     const method = req.params.method;
-    const catalogoBandas = await Catalogo.findAll();
+    const catalogoBandas = await Catalogo.findAll(orderById);
     const bandas = await Catalogo.findByPk(req.params.id);
 
     if (method == "put") {
@@ -44,12 +63,16 @@ const getById = async (req, res) => {
         catalogoBandas,
         bandasPut: bandas,
         bandasDel: null,
+        message,
+        type,
       });
     } else {
       res.render("index", {
         catalogoBandas,
         bandasPut: null,
         bandasDel: bandas,
+        message,
+        type,
       });
     }
   } catch (err) {
@@ -61,6 +84,8 @@ const update = async (req, res) => {
   try {
     const bandas = req.body;
     await Bandas.update(bandas, { where: { id: req.params.id } });
+    message = "Banda atualizada com sucesso!";
+    type = "success";
     res.redirect("/");
   } catch (err) {
     res.status(500).send({ err: err.message });
@@ -70,6 +95,8 @@ const update = async (req, res) => {
 const remove = async (req, res) => {
   try {
     await Catalogo.destroy({ where: { id: req.params.id } });
+    message = "Banda removida com sucesso!";
+    type = "success";
     res.redirect("/");
   } catch (err) {
     res.status(500).send({ err: err.message });
