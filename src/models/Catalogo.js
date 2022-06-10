2da6f688edef17bfc5a33493e3cf047cd046e57a
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-06-09 22:07:46 -0300

    init table

--- src/models/Catalogo.js
+++ src/models/Catalogo.js
@@ -1,36 +1,44 @@
 const Sequelize = require("sequelize");
 const connection = require("../database/db");
 
-const Bandas = connection.define("bandas", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
+const Bandas = connection.define(
+  "bandas",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    nome: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    descricao: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    estilo: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    imagem: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
   },
-  nome: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  descricao: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  estilo: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  imagem: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-},
-{
+  {
     freezeTableName: true,
     timestamps: false,
     createdAt: false,
     updatedAt: false,
-}
+  }
 );
+
+const initTable = async () => {
+  await Bandas.sync();
+};
+
+initTable();
 
 module.exports = Bandas;
